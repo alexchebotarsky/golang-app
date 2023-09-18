445fb34d07b22fec8a71d25acbf40c869255dd53
goodleby <goodleby@gmail.com>
2023-09-18 07:44:52 +0200

    Update dependencies

--- client/auth/auth.go
+++ client/auth/auth.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/goodleby/golang-server/config"
 	"github.com/goodleby/golang-server/tracing"
 )
