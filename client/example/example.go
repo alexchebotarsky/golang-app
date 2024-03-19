91b6e2c21c7279b3a121e1287f4fc8a63fab0c5e
goodleby <goodleby@gmail.com>
2024-03-19 17:49:24 +0100

    Refactor example client

--- client/example/example.go
+++ client/example/example.go
@@ -15,17 +15,16 @@ type Client struct {
 	HTTPClient      *http.Client
 }
 
-func New(endpoint string) (*Client, error) {
-	c := &Client{}
+func New(endpoint string) *Client {
+	var c Client
 
 	c.ExampleEndpoint = endpoint
-
 	c.HTTPClient = &http.Client{
 		Timeout:   3 * time.Second,
 		Transport: tracing.NewTracedTransport(http.DefaultTransport),
 	}
 
-	return c, nil
+	return &c
 }
 
 type ExampleData struct {
@@ -36,29 +35,29 @@ type ExampleData struct {
 	Gender    string `json:"gender"`
 }
 
-func (c *Client) FetchExampleData(ctx context.Context) (*ExampleData, error) {
+func (c *Client) FetchExampleData(ctx context.Context) (ExampleData, error) {
 	ctx, span := tracing.StartSpan(ctx, "FetchExampleData")
 	defer span.End()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.ExampleEndpoint, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error creating new request: %v", err)
+		return ExampleData{}, fmt.Errorf("error creating new request: %v", err)
 	}
 
 	res, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error doing http request: %v", err)
+		return ExampleData{}, fmt.Errorf("error doing http request: %v", err)
 	}
 
 	if res.StatusCode >= 400 {
-		return nil, fmt.Errorf("received non-successful status code: %s", res.Status)
+		return ExampleData{}, fmt.Errorf("received non-successful status code: %s", res.Status)
 	}
 
 	var exampleData ExampleData
 	err = json.NewDecoder(res.Body).Decode(&exampleData)
 	if err != nil {
-		return nil, fmt.Errorf("error decoding example data: %v", err)
+		return ExampleData{}, fmt.Errorf("error decoding example data: %v", err)
 	}
 
-	return &exampleData, nil
+	return exampleData, nil
 }
